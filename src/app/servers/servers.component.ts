58bddf7ac3bc538d53ea2392fb100ef077c7a3b7
Nathan Kruckenberg <natekruck1990@gmail.com>
2022-02-06 15:11:49 -0600

    input component added

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,18 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer:boolean = false
   serverCreationStatus: string = 'No server was created';
+  serverName = '';
 
   constructor() { 
     setTimeout(() => {
       this.allowNewServer = true
     }, 2000)
-   }
-
+  }
+  
   ngOnInit(): void {
   }
-
+  
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
   }
 
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  
 }
